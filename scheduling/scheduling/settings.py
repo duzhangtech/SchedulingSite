d4ca09ed16dc7d318f4bfa1b11da297788c3b299
Yu Sun <yusun@wustl.edu>
2013-12-19 22:49:08 -0600

    login feature

--- scheduling/scheduling/settings.py
+++ scheduling/scheduling/settings.py
@@ -10,8 +10,13 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+
+PROJECT_DIRECTORY = os.getcwd()
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
+TEMPLATE_DIRS = (
+    os.path.join(PROJECT_DIRECTORY,'templates/'),
+)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
--- scheduling/scheduling/urls.py
+++ scheduling/scheduling/urls.py
@@ -1,12 +1,14 @@
 from django.conf.urls import patterns, include, url
-
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'scheduling.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
-    url(r'^admin/', include(admin.site.urls)),
+	url(r'^$',  'scheduling.views.index'),
+	url(r'^auth/$',  'scheduling.views.auth_view'),
+	url(r'^logout/$', 'scheduling.views.logout'),
+	url(r'^loggedin/$', 'scheduling.views.loggedin'),
+	url(r'^invalid/$', 'scheduling.views.invalid_login'),
+	# registration   
+	url(r'^register/$', 'scheduling.views.register_user'),
+	url(r'^register_success/$', 'scheduling.views.register_success'),
 )
